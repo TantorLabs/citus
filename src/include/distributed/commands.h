2f83746f5642a03ef687d520f82480de44acda6b
Ahmet Gedemenli <afgedemenli@gmail.com>
2022-07-21 13:47:31 +0300

    Revert CreateMaterializedViewDDLCommand

--- src/include/distributed/commands.h
+++ src/include/distributed/commands.h
@@ -570,7 +570,6 @@ extern List * AlterViewStmtObjectAddress(Node *node, bool missing_ok);
 extern List * PreprocessDropViewStmt(Node *node, const char *queryString,
 									 ProcessUtilityContext processUtilityContext);
 extern char * CreateViewDDLCommand(Oid viewOid);
-extern char * CreateMaterializedViewDDLCommand(Oid matViewOid);
 extern List * GetViewCreationCommandsOfTable(Oid relationId);
 extern List * GetViewCreationTableDDLCommandsOfTable(Oid relationId);
 extern char * AlterViewOwnerCommand(Oid viewOid);
